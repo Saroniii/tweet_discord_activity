d05f3af4bcdbd60485906dfa3dcd57c6122ac521
Saroniii <sarotube0211@gmail.com>
2020-11-12 13:47:32 +0900

    Add Startup class

---
+++ utils/startup.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from utils import StartupError, Check as check
+
+class startup:
+
+    def __init__(self):
+        self.check = check()
+
+    async def setup(self, bot :commands.Bot):
+        """
+        Startup the bot.
+        """
+        
+        owner = await self.check.get_bot_owner(bot)
+        startup_check = await self.check.can_startup(owner, bot)
+
+        if not startup_check[0]:
+            raise StartupError(startup_check[1])
+
+        return owner
