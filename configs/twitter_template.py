0e327351955d927964e91e76c7f23973623f52ba
Saroniii <sarotube0211@gmail.com>
2020-11-14 19:27:17 +0900

    Add twitter_template for easy use

--- configs/twitter_template.py
+++ configs/twitter_template.py
@@ -2,3 +2,8 @@ CONSUMER_KEY = "YOUR CONSUMER KEY HERE"
 CONSUMER_SECRET = "YOUR CONSUMER KEY SECRET HERE"
 ACCESS_TOKEN = "YOUR ACCESS TOKEN HERE"
 ACCESS_TOKEN_SECRET = "YOUR ACCESS TOKEN SECRET HERE"
+
+CHANGE_STATUS_TWEET = "(owner) has changed the status of the Discord to (status)."
+START_ACTIVITY_TWEET = "(owner) has started the activity of Discord to (acrivity)."
+END_ACTIVITY_TWEET = "(owner) has ended (activity) of Discord."
+ACTIVITY_BOT_START_TWEET = "Check activity bot was started."
