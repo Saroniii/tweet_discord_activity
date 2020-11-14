4a9d130622044687d8a5018059f0eedf2e0e7aa1
Saroniii <sarotube0211@gmail.com>
2020-11-14 19:29:40 +0900

    Add datetime

--- utils/tweet.py
+++ utils/tweet.py
@@ -3,6 +3,9 @@ from aiohttp.client import ClientSession
 import aiohttp_oauth_client 
 from authlib.client.aiohttp import AsyncOAuth1Client, OAuthRequest
 import configs.twitter as twitter
+import discord
+from datetime import date, datetime
+import pytz
 
 class Tweet:
 
@@ -25,9 +28,40 @@ class Tweet:
                 token_secret=twitter.ACCESS_TOKEN_SECRET
                 )
             
-            params = {"status":"hello world"}
+            params = {"status":sentence}
 
-            await client.post(
+            tes = await client.post(
                 url=post_url,
-                params=params
+                data=params
                 )
+            print(tes)
+
+    def replace_variable(self, sentence: str, owner: discord.User, activity: str ='None', status: str ='None'):
+        time = datetime.now()
+        vars_dict = {
+            '(owner)': str(owner),
+            '(owner_name)': owner.name,
+            '(owner_discriminator)': str(owner.discriminator),
+            '(owner_id)': str(owner.id),
+            '(status)': status,
+            '(activity)': activity,
+            '(time)': f"{time.hour}:{time.second}",
+            '(time_month_and_day)': f"{time.month}/{time.day}",
+            '(time_year)': str(time.year),
+            '(time_month)': str(time.month),
+            '(time_day)': str(time.day),
+            '(time_hour)': str(time.hour),
+            '(time_minute)': str(time.minute),
+            '(time_second)': str(time.second)
+        }
+
+        for before, after in vars_dict.items():
+            sentence = sentence.replace(before, after)
+
+        return sentence
+
+    def get_timezone(self):
+        tz = twitter.TIMEZONE
+        if not tz:
+            tz = 'utc'
+        return pytz.timezone(tz)
