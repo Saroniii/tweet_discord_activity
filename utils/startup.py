b0746037068ce05772100c3669ff996b6b9b3ab9
Saroniii <sarotube0211@gmail.com>
2020-11-16 23:22:05 +0900

    Create get_token function

--- utils/startup.py
+++ utils/startup.py
@@ -1,6 +1,8 @@
+from configs.token import TOKEN
 import discord
 from discord.ext import commands
 from utils import StartupError, Check as check
+import os
 
 class Startup:
 
@@ -19,3 +21,25 @@ class Startup:
             raise StartupError(startup_check[1])
 
         return owner
+
+    def get_token(self) -> str:
+        """
+        Get token.
+        """
+
+        try:
+            token = os.environ["TOKEN"]
+        except:
+            pass
+
+        if not token:
+            
+            try:
+                import configs.token
+                token = configs.token.TOKEN
+                if len(token) == 0:
+                    raise StartupError('Activity bot token was not found.')
+            
+            except:
+                raise StartupError('Activity bot token was not found.')
+
