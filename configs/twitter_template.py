3d55779d96a7ece0ef661d7590e84f02bf77d0ee
Saroniii <sarotube0211@gmail.com>
2020-11-21 10:54:07 +0900

    Remove unused template sentence

--- configs/twitter_template.py
+++ configs/twitter_template.py
@@ -6,6 +6,5 @@ ACCESS_TOKEN_SECRET = "YOUR ACCESS TOKEN SECRET HERE"
 CHANGE_STATUS_TWEET = "(owner) has changed the status of the Discord to (status)."
 START_ACTIVITY_TWEET = "(owner) has started the activity of Discord to (activity)."
 END_ACTIVITY_TWEET = "(owner) has ended (activity) of Discord."
-ACTIVITY_BOT_START_TWEET = "Check activity bot was started."
 
 LANG = 'en'
